b41d98e1d8937c4bc5a04aafb466d220c610f1f7
huan.geng <gh1731360062@163.com>
2023-04-27 15:28:08 +0800

    fix

--- src/main/java/com/apache/platform/PlatformApplication.java
+++ src/main/java/com/apache/platform/PlatformApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("com.apache.platform.dao")
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.apache.platform")
 public class PlatformApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/apache/platform/controller/OrderController.java
+++ src/main/java/com/apache/platform/controller/OrderController.java
@@ -4,22 +4,22 @@ package com.apache.platform.controller;
 import com.apache.platform.dto.OrderListDto;
 import com.apache.platform.service.IOrderService;
 import com.apache.platform.vo.OrderListVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
 @RequestMapping("order")
 public class OrderController {
 
-    @Resource
+    @Autowired
     private IOrderService orderService;
 
 
-    @GetMapping("")
+    @GetMapping("/index")
     public List<OrderListVo> getList(OrderListDto orderListDto) {
         return orderService.getList(orderListDto);
     }
